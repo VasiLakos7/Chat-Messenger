d87df9014d6baf59d4dd2d4682019fe071784544
Vasilis Rantzos <116198356+VasiLakos7@users.noreply.github.com>
2024-02-09 20:13:14 +0200

    Add files via upload

---
+++ ChatMessenger.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chat.messenger;
+
+/**
+ *
+ * @author ArKive
+ */
+
+public class ChatMessenger {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Sign_in frame = new Sign_in();
+            frame.setVisible(true);   
+    }
+    
+}
